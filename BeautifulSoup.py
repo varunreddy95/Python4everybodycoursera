53263ff57acc0a5fe5fbe81fedd67dd309c1aedb
Varun Reddy <varunreddy_95@outlook.com>
2020-07-03 00:57:55 +0530

    Web Scrapping with BeautifulSoup and retrieving anchor tags

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -1,9 +1,15 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
+import ssl
 
 url = input('Enter - ')
 html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')
+
+#Ignore SSL certificate erors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 #Retrieve all of the anchor tags
 tags = soup('a')
